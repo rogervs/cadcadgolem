e4b4d0db1811e4f5e81a5b80fd8cc9454fcab07d
rogervs <rogervs@protonmail.com>
2020-12-30 19:27:23 +0000

    Update MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cadcadgolem',
-    version='0.1.1',
+    version='0.1.2',
     author='Roger van Schie',
     author_email='rogervs@protonmail.com',
     description='A cadCAD workload dispatcher to the Golem network',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/rogervs/cadcadgolem',
-    packages=setuptools.find_packages(include=['cadcadgolem']),
-    install_requires=['cadcad','yapapi'],
+    ackages=setuptools.find_packages(include=['cadcadgolem']),
+    install_requires=[
+        'cadcad',
+        'yapapi'
+    ],
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
